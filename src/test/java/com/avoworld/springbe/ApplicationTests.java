b6dc59ba82bef879ae44c3fd706819313398a04e
moolmin <zzmzzm0065@gmail.com>
2024-06-13 01:15:32 +0900

    Connect with MySQL

---
+++ src/test/java/com/avoworld/springbe/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avoworld.springbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
