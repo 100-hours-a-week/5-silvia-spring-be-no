a8ffcad94229a633e34cc38027ac36c01f86c918
moolmin <zzmzzm0065@gmail.com>
2024-06-13 15:52:13 +0900

    Fix MySQL Connection

--- src/main/java/com/avoworld/springbe/exception/PostNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.avoworld.springbe.exception;
-
-public class PostNotFoundException extends RuntimeException {
-    public PostNotFoundException(Long postId) {
-        super("Post not found with id " + postId);
-    }
-}
